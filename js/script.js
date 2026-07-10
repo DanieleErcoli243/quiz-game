f9cbba5f78eff9dd626833045a92d6600791ace3
danieleercoli243 <danielercoli@gmail.com>
2026-07-10 11:11:49 +0200

    got the element from the DOM

--- js/script.js
+++ js/script.js
@@ -1,1 +1,17 @@
-console.log("js Ok");
+// seleziono gli elementi dal DOM
+
+const startScreen = document.getElementById('start-screen');
+const quizScreen = document.getElementById('quiz-screen');
+const resultScreen = document.getElementById('result-screen');
+const startBtn = document.getElementById('start-btn');
+const questionText = document.getElementById('question-text');
+const answersContainer = document.getElementById('answers-container');
+const currentQuestionSpan = document.getElementById('current-question');
+const totalQuestionSpan = document.getElementById('total-question');
+const scoreSpan = document.getElementById('score');
+const finalScoreSpan = document.getElementById('final-score');
+const maxScoreSpan = document.getElementById('max-score');
+const resultMessage = document.getElementById('result-message');
+const restartBtn = document.getElementById('restart-btn');
+const progressBar = document.getElementById('progress');
+
